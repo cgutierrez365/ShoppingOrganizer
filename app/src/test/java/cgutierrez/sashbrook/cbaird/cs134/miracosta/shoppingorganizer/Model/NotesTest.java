a3ce2c44456123acb2bd91592e97891c41d8722f
Clarissa <clarissag2142@gmail.com>
2019-05-21 15:26:47 -0700

    Slightly adjusted custom_store to wrap content. Added NotesTest (I still need to fill it in)

---
+++ app/src/test/java/cgutierrez/sashbrook/cbaird/cs134/miracosta/shoppingorganizer/Model/NotesTest.java
@@ -0,0 +1,40 @@
+package cgutierrez.sashbrook.cbaird.cs134.miracosta.shoppingorganizer.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotesTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getNoteTitle() {
+    }
+
+    @Test
+    public void setNoteTitle() {
+    }
+
+    @Test
+    public void getNoteContents() {
+    }
+
+    @Test
+    public void setNoteContents() {
+    }
+
+    @Test
+    public void describeContents() {
+    }
+
+    @Test
+    public void writeToParcel() {
+    }
+}

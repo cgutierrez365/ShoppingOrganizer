8ed46bb6f74ffdd69b8ac881cf09d07c8e8ba496
Clarissa <clarissag2142@gmail.com>
2019-05-21 15:54:30 -0700

    Added AddStoreActivity (not finished yet)

---
+++ app/src/main/java/cgutierrez/sashbrook/cbaird/cs134/miracosta/shoppingorganizer/AddStoreActivity.java
@@ -0,0 +1,13 @@
+package cgutierrez.sashbrook.cbaird.cs134.miracosta.shoppingorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_store);
+    }
+}

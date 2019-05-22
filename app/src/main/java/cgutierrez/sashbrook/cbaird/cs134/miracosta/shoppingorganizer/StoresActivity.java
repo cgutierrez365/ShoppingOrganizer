3c15e1596e9858be8e7056f975ed135b17a84069
Clarissa <clarissag2142@gmail.com>
2019-05-22 11:21:55 -0700

    Fixed StoresActivity onCreate() which should be linked to activity_stores.xml and not activity_add_store.xml

--- app/src/main/java/cgutierrez/sashbrook/cbaird/cs134/miracosta/shoppingorganizer/StoresActivity.java
+++ app/src/main/java/cgutierrez/sashbrook/cbaird/cs134/miracosta/shoppingorganizer/StoresActivity.java
@@ -60,7 +60,7 @@ public class StoresActivity extends AppCompatActivity {
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_store);
+        setContentView(R.layout.activity_stores);
 
 
         Button addStore = findViewById(R.id.addStore);
